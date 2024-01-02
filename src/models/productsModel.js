a51c119c1785340a97690b3efa27f18c50d6ab41
GabrielAlberini <77715611+GabrielAlberini@users.noreply.github.com>
2024-01-02 17:31:44 -0300

    pathfile fix

--- src/models/productsModel.js
+++ src/models/productsModel.js
@@ -1,5 +1,6 @@
 import fs from "node:fs";
 import products from "../database/products.json" assert { type: "json" };
+import { pathfile } from "../database/index.js";
 
 export const getProducts = () => {
   try {
@@ -12,7 +13,7 @@ export const getProducts = () => {
 export const addProduct = (newProduct) => {
   try {
     products.push(newProduct);
-    fs.writeFileSync("./src/database/products.json", JSON.stringify(products));
+    fs.writeFileSync(pathfile + "/products.json", JSON.stringify(products));
   } catch (error) {
     throw new Error("406 - NOT ACCEPTABLE", error);
   }
